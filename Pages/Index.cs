8e805a9ef816e769c3f28a298ca912b89efb652d
jakob <aajakobsung@gmail.com>
2025-09-16 23:57:59 +0900

    Add Anton font, update MainPage.xaml, add VS Code debug configs and COPILOT guidelines, add .gitignore

---
+++ Pages/Index.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System.Threading.Tasks;
+using OpenSilver.WebAssembly;
+
+namespace Awes_Homepage.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            await Runner.RunApplicationAsync<Awes_Homepage.App>();
+        }
+    }
+}

89e3be9186fa2c2f42592de2a97cda8b02052a90
jakob <aajakobsung@gmail.com>
2025-09-28 18:24:38 +0900

    chore: add full solution (browser, shared library, simulator)

---
+++ Awes_Homepage.Simulator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using OpenSilver.Simulator;
+using System;
+
+namespace Awes_Homepage.Simulator
+{
+    internal static class Startup
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            return SimulatorLauncher.Start(typeof(App));
+        }
+    }
+}
